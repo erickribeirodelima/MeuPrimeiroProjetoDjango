b5b72d342d1e61d98abef6687725548deac82f8a
erick.ribeiro <eri.05@hotmail.com>
2022-07-11 22:12:02 -0300

    alteraçao model

--- Rh/models.py
+++ Rh/models.py
@@ -1,7 +1,7 @@
 from multiprocessing import set_forkserver_preload
 from django.db import models
 
-# Create your models here.
+# Crie seus modelos aqui
 class Departamento(models.Model):
     nome = models.CharField(max_length=20)
 
